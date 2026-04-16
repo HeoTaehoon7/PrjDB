717985ed96e272dd354ab80d0f3cc35e3624936a
HeoTaehoon7 <greenclass.hoon@gmail.com>
2026-04-16 14:48:39 +0900

    crud 2

--- src/db3/TestTUser.java
+++ src/db3/TestTUser.java
@@ -3,6 +3,7 @@ package db3;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
@@ -19,6 +20,9 @@ public class TestTUser {
 		// CRUD 예제, Create, Read, Update, Delete
 		do {
 			// 화면춣력
+			//System.out.print("\033[H\033[2J"); // \033[H: 커서를 화면 왼쪽 상단(Home)으로 이동
+			//System.out.flush();                // \033[2J: 화면 전체를 지움
+			
 			System.out.println("========================");
 			System.out.println("       회원 정보        ");
 			System.out.println("========================");
@@ -32,13 +36,21 @@ public class TestTUser {
 			System.out.println("선택:");
 			String    choice  =   in.nextLine();
 			
+			TUserDTO   tuser = null;
+			
 			switch( choice ) {
-			case  "1":  // 회원목록         
+			case  "1":  // 회원 목록 
+				
 				break;
-			case  "2":  // 회원 조회 
+			case  "2":  // 회원 조회 (아이디)
+				System.out.println("조회할 아이디를 입력하세요");
+				String     uid    =  in.nextLine();
+				tuser             =  getTUser( uid );
+				// System.out.println( tuser.toString() );
+				display( tuser );
 				break;
 			case  "3":  // 회원 추가
-				TUserDTO   tuser  =  inputData();
+				tuser             =  inputData();
 				int        aftcnt = addTUser( tuser );
 				System.out.println(aftcnt + "건 저장되었습니다");
 				break;
@@ -55,6 +67,47 @@ public class TestTUser {
 		} while( true );  // 무한반복 : 무한루프		
 		
 
+	}
+
+	// 입력받은 아이디로 한줄을 db 에서 조회한다
+	private static TUserDTO getTUser(String uid) 
+			throws ClassNotFoundException, SQLException {
+		Class.forName( driver );
+		Connection          conn   =  DriverManager.getConnection(url, dbuid, dbpwd);
+		String              sql    =  " SELECT * FROM TUSER  WHERE  USERID = ?";
+		PreparedStatement   pstmt  =  conn.prepareStatement( sql );
+		pstmt.setString(1, uid );
+		
+		TUserDTO  tuser = null;
+		
+		ResultSet           rs     =  pstmt.executeQuery();
+		if( rs.next() ) {  // 해당자료가 있다
+			String  userid    = rs.getString("userid");
+			String  username  = rs.getString("username");
+			String  email     = rs.getString("email");
+			
+			tuser             =  new TUserDTO(userid, username, email)  ;
+			
+		} else { // 해당자료가 없다 : primary key
+			
+		}		
+		
+		pstmt.close();
+		conn.close();
+		
+		return tuser;
+	}
+
+	// TUser 한줄을 출력한다
+	private static void display(TUserDTO tuser) {
+		
+		if( tuser == null ) 
+			System.out.println("조회한 자료가 없습니다");
+		else {
+			String msg = String.format("%s %s %s",
+				tuser.getUserid(), tuser.getUsername(), tuser.getEmail());
+			System.out.println( msg );
+		}
 	}
 
 	// db 에 insert 한다
